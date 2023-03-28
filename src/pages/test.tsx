f7752fdf9583f3e2ef47cf15cb32f4257d9ba838
betterplaywon <kkgi1234@gmail.com>
2023-03-28 23:59:24 +0900

    add: test 컴포넌트 생성

---
+++ src/pages/test.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./components/Header";
+
+const test = () => {
+  return (
+    <>
+      <Header />
+      <div>라우팅 테스트 페이지임</div>
+    </>
+  );
+};
+
+export default test;
