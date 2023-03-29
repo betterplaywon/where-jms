8ad63defa1e26b1f2408f1f0b44e075f9750392f
betterplaywon <kkgi1234@gmail.com>
2023-03-29 18:31:44 +0900

    add: Home component 설정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,42 @@
+import { useEffect } from "react";
 import Head from "next/head";
 import Image from "next/image";
+import Header from "@/pages/components/Header";
+import MapPart from "@/pages/components/home/MapPart";
+import { Churches } from "@/types/churches";
+import useChurches from "@/hooks/useChurches";
+import { NextPage } from "next";
 
-export default function Home() {
-  return <>JMS</>;
+interface Props {
+  stores: Churches[];
+}
+
+const Home: NextPage<Props> = ({ churches }) => {
+  const { initializeChurches } = useChurches();
+
+  useEffect(() => {
+    initializeChurches(churches);
+  }, [initializeChurches, churches]);
+
+  console.log(churches);
+
+  return (
+    <>
+      <Header />
+      <main style={{ width: "100%", height: "100%" }}>
+        <MapPart />
+      </main>
+    </>
+  );
+};
+
+export default Home;
+
+export async function getStaticProps() {
+  const churches = await (await import("../../public/churches.json")).default;
+
+  return {
+    props: { churches },
+    // 데이터 변동이 거의 없을거라 생각해 revalidate값은 추가하지 않음
+  };
 }
