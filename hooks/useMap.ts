721e06d680cfb0c1e2ac11c9f362019fc9b5a88b
betterplaywon <kkgi1234@gmail.com>
2023-04-03 09:22:33 +0900

    add: useMap > URL의 좌표와 zoom에 따른 옵션 가져오기 & 리셋 함수 생성

--- hooks/useMap.ts
+++ hooks/useMap.ts
@@ -1,7 +1,7 @@
 import { NaverMap } from "@/types/map";
 import { useCallback } from "react";
-import { mutate } from "swr";
-import { Coordinates } from "../types/churches";
+import useSWR, { mutate } from "swr";
+import { Coordinates } from "@/types/churches";
 
 export const INITIAL_CENTER: Coordinates = [37.5262411, 126.99289439];
 export const INITIAL_ZOOM = 10;
@@ -9,12 +9,27 @@ export const INITIAL_ZOOM = 10;
 export const MAP_KEY = "/map";
 
 const useMap = () => {
+  const { data: map } = useSWR(MAP_KEY);
+
   const initializeMap = useCallback((map: NaverMap) => {
     mutate(MAP_KEY, map);
   }, []);
 
+  const resetMapOptions = useCallback(() => {
+    map.morph(new naver.maps.LatLng(...INITIAL_CENTER), INITIAL_ZOOM);
+  }, [map]);
+
+  const getMapOptions = useCallback(() => {
+    const mapCenter = map.getCenter();
+    const zoom = map.getZoom();
+    const center: Coordinates = [mapCenter.lat(), mapCenter.lng()];
+    return { center, zoom };
+  }, [map]);
+
   return {
     initializeMap,
+    resetMapOptions,
+    getMapOptions,
   };
 };
 
