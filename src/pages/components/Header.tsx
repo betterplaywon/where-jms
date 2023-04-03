1deeb38c29b3a85e210e69e6c64ac81ee688d676
betterplaywon <kkgi1234@gmail.com>
2023-04-03 09:24:21 +0900

    add: map 정보를 담은 query로 route 처리

--- src/pages/components/Header.tsx
+++ src/pages/components/Header.tsx
@@ -1,14 +1,30 @@
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useCallback } from "react";
 import { BsShare } from "react-icons/bs";
 import { VscFeedback } from "react-icons/vsc";
+import useMap from "@/hooks/useMap";
+import { useRouter } from "next/router";
+import copy from "copy-to-clipboard";
 
 const Header = () => {
+  const { resetMapOptions, getMapOptions } = useMap();
+  const router = useRouter();
+
+  const changeAndCopyUrl = useCallback(() => {
+    // 현재 지도의 options를 가져와서
+    const mapOptions = getMapOptions();
+    // query를 만들고, 해당 query를 이용해 routing한다
+    const query = `/?zoom=${mapOptions.zoom}&lat=${mapOptions.center[0]}&lng=${mapOptions.center[1]}`;
+    router.replace(query);
+    copy(location.origin + query);
+  }, [router, getMapOptions]);
+
   return (
     <header className="absolute top-0 left-0 w-full h-2 px-4 flex justify-between items-center z-50 mt-6 p-3">
       <nav className="drop-shadow-lg">
         <Link
+          onClick={resetMapOptions}
           className="flex items-center text-black shrink-0 mr-5 bg-white"
           href="/"
         >
@@ -16,7 +32,10 @@ const Header = () => {
         </Link>
       </nav>
       <nav className="flex items-center gap-3 text-sm text-center">
-        <button className="mr-1 p-4 bg-white drop-shadow-md">
+        <button
+          className="mr-1 p-4 bg-white drop-shadow-md"
+          onClick={changeAndCopyUrl}
+        >
           <BsShare />
         </button>
         <button className="p-4 bg-white drop-shadow-md">
